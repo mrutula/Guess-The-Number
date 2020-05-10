8750197adc755eca83145dcfd451fe44ee598f73
mrutulasuresh <62486701+mrutulasuresh@users.noreply.github.com>
2020-05-10 22:17:39 +1000

    Update GuessTheNumber.py
    Class to check the user guessed number

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,2 +1,53 @@
-msg = "Hello World"
-print(msg)
+import random
+import sys
+
+class GuessTheNumber:  
+    """
+    This is a class to compare user's guess with the randomly generated number
+    
+    Attributes:
+        guess (int): User's guess
+    """ 
+    def __init__(self, guess):
+        self.guess = guess
+    
+    def randomgenerator(self):
+        """
+        The function to generate a random number between 1 and 20
+
+        Return:
+            number: random number generated
+        """
+        number = random.randint(1, 20)
+        return number
+    
+    def isguessright(self):
+        """The function to check if user guess is right"""
+        number = self.randomgenerator()
+        if self.guess < number:
+            print(f"Your guess {self.guess} is too low")
+        elif self.guess > number:
+            print(f"Your guess {self.guess} is too high")
+        else:
+            print(f"You guessed it right!!!!")
+
+if __name__ == '__main__':
+    while True:
+        user_input = input("Enter a number between 1 and 20\t")
+        if user_input == 'quit':
+            break
+        try:
+            user_num = int(user_input)
+            guess = GuessTheNumber(int(user_num))
+            guess.isguessright()
+        except ValueError:
+            print(f"This is not a valid number. It isnt a number at all! This is a string, go and try again, Better luck next time")
+
+
+
+
+
+
+
+
+
